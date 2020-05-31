bbdc8c1afac24965ee8753487bbbe1dc42e2b4f2
Stanislaw Cruz <stanycruz@outlook.com>
2020-05-31 15:29:26 -0300

    Consumindo um serviço.

--- Hosting/Program.cs
+++ Hosting/Program.cs
@@ -1,11 +1,7 @@
 using Passagens;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.ServiceModel;
 using System.ServiceModel.Description;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hosting
 {
